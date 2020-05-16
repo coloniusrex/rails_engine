fb22efa11463c90f616f7dc34c8f82ebbb928aee
Colin Alexander <colintalex@gmail.com>
2020-05-15 21:56:36 -0600

    Creates model tests and updates relations/validations

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,4 +1,9 @@
 class Invoice < ApplicationRecord
+  validates :status, presence: true
+
   belongs_to :merchant
   belongs_to :customer
+
+  has_many :invoice_items, dependent: :destroy
+  has_many :items, through: :invoice_items
 end
